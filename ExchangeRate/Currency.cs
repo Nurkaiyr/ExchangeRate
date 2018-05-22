61730f459f7dc157e7437c1f4bd65c500f24377b
Nurkaiyr <yedige21@gmail.com>
2018-05-22 16:42:01 +0600

    істелінді

---
+++ ExchangeRate/Currency.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace ExchangeRate
+{
+    public class Currency
+    {
+        [JsonProperty("id")]
+        public string Id { get; set; }
+        [JsonProperty("name_kaz")]
+        public string NameKaz { get; set; }
+        [JsonProperty("edinica_izmerenia")]
+        public string EdinicaIzmerenia { get; set; }
+        [JsonProperty("name_rus")]
+        public string NameRus { get; set; }
+        [JsonProperty("kurs")]
+        public string Kurs { get; set; }
+        [JsonProperty("kod")]
+        public string Kod { get; set; }
+    }
+}
---
+++ ExchangeRate/MainWindow.xaml.cs
@@ -0,0 +1,55 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ExchangeRate
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void RefreshButton_Click(object sender, RoutedEventArgs e)
+        {
+            Thread thread = new Thread(() =>
+            {
+                string url = @"http://data.egov.kz/api/v2/valutalar_bagamdary4/v1";
+                string json;
+
+                using (WebClient wc = new WebClient())
+                {
+                    wc.Encoding = Encoding.UTF8;
+                    json = wc.DownloadString(url);
+                }
+
+                List<Currency> currencyList = JsonConvert.DeserializeObject<List<Currency>>(json);
+                Thread.Sleep(5000);
+
+                DataGrid.Dispatcher.Invoke(new Action(() =>
+                {
+                    DataGrid.ItemsSource = currencyList;
+                }));
+            });
+            thread.Start();
+        }
+    }
+}
